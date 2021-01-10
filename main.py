8221763c00c26073033ec9b9ecf6799bd766562e
mihnea <13464649+umihnea@users.noreply.github.com>
2021-01-10 18:32:53 +0200

    Blank notebook

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import csv
-
-import requests
-import numpy as np
 import pandas as pd
+import requests
 from datetime import datetime
-
-eps = np.finfo(float).eps
-from numpy import log2 as log
 
 URL = "https://store.steampowered.com/appreviews/"
 APPID = "1151640"
 
 PARAMS = {'json': 1, "cursor": "*"}
+DATA_DIR = 'data'
 
 
 def dict_flatten(entry):
@@ -48,54 +43,33 @@ def parse_row(entry):
     entry['written_during_early_access'] = bool(entry['written_during_early_access'])
     return entry
 
-def generate_data(appid):
+
+def generate_data(appid, review_fn, summary_fn):
     index = 0
     reviews = []
     query_summary = None
-    while index < 1000:
+    while index < 50:
         r = requests.get(url=URL + appid, params=PARAMS)
         data = r.json()
         if query_summary is None:
             query_summary = data['query_summary']
         reviews += data['reviews']
         index += data['query_summary']['num_reviews']
-        PARAMS['cursor'] = data['cursor']
-        print("INDEX " + str(index))
-    fields = ['recommendationid', 'author_steamid', 'author_num_games_owned', 'author_num_reviews',
-              'author_playtime_forever', 'author_playtime_last_two_weeks', 'author_playtime_at_review',
-              'author_last_played', 'language', 'review', 'timestamp_created', 'timestamp_updated', 'voted_up',
-              'votes_up', 'votes_funny', 'weighted_vote_score', 'comment_count', 'steam_purchase', 'received_for_free',
-              'written_during_early_access', 'developer_response', 'timestamp_dev_responded']
-    summary_fields = ['num_reviews', 'review_score', 'review_score_desc', 'total_positive', 'total_negative', 'total_reviews']
-    query_summary['num_reviews'] = index
-    with open('summary' + appid + '.csv', 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=summary_fields)
-        writer.writeheader()
-        writer.writerow(query_summary)
-    with open('review' + appid + '.csv', 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
-        writer.writeheader()
-        for entry in reviews:
-            try:
-                writer.writerow(dict_flatten(entry))
-            except UnicodeEncodeError:
-                pass
-    print(reviews)
 
-def open_data(appid):
-    reviews = []
-    summary = None
-    with open('review' + appid + '.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            reviews.append(parse_row(row))
-    with open('summary' + appid + '.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            summary = row
-    return reviews, summary
+        print("index " + str(index))
+        PARAMS['cursor'] = data['cursor']
+
+    query_summary['num_reviews'] = index
+
+    pd.DataFrame(query_summary, index=[0]).to_csv(summary_fn)
+    reviews = pd.DataFrame.from_records(
+        [dict_flatten(review) for review in reviews]
+    )
+    reviews.to_csv(review_fn)
+
 
 if __name__ == "__main__":
-    # generate_data(APPID)
-    reviews, summary = open_data(APPID)
-    print(summary)
+    review_fn = '%s/review_%s.csv' % (DATA_DIR, APPID)
+    summary_fn = '%s/summary_%s.csv' % (DATA_DIR, APPID)
+
+    generate_data(APPID, review_fn, summary_fn)
